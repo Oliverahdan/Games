ed9231b28669cd1f778269e328172dbdbeffc37b
Danram <dos03042003@gmail.com>
2024-01-10 17:33:01 -0300

    dia 3

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql2/promise');
-
+const bodyParser = require('body-parser');
 
 // Cria uma instância do Express
 const app = express();
@@ -15,6 +15,13 @@ const dbConfig = {
     database: 'gamedan'
 };
 
+// Função para criar e retornar uma nova conexão
+async function createConnection() {
+    return await mysql.createConnection(dbConfig);
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/descricao', async (req, res) => {
     try {
@@ -26,10 +33,9 @@ app.get('/descricao', async (req, res) => {
 
         console.log('Recebido pedido para o jogo com ID:', gameId);
 
-        const connection = await mysql.createConnection(dbConfig);
+        const connection = await createConnection();
         const [rows, fields] = await connection.execute('SELECT * FROM games WHERE id = ?', [gameId]);
-        await connection.end();
-
+        
         if (rows.length === 0) {
             console.log('Jogo não encontrado no banco de dados');
             return res.status(404).send('Jogo não encontrado');
@@ -39,9 +45,54 @@ app.get('/descricao', async (req, res) => {
 
         // Renderiza a página descricao.html com os dados do jogo
         res.sendFile(path.join(__dirname, '/descricao/index.html'));
+
+        // Não fechamos a conexão aqui para que ela esteja disponível para outras rotas
     } catch (error) {
         console.error('Erro na consulta ao banco de dados:', error);
         res.status(500).send('Erro interno no servidor');
+    }
+});
+
+// API endpoint for user login
+app.post('/login', (req, res) => {
+    const { nome, email } = req.body;
+  
+    // Retrieve user data from the 'users' table
+    connection.query('SELECT * FROM users WHERE name = ? AND password = ?', [nome, email], (err, results) => {
+      if (err) {
+        return res.status(500).json({ message: 'Erro no servidor' });
+      }
+  
+      if (results.length === 0) {
+        return res.status(401).json({ message: 'Credenciais inválidas' });
+      }
+
+  
+      // Redirecione o usuário para a página de grupo
+      res.redirect('/jogos');
+    });
+  });
+
+  app.get('/api/jogos', async (req, res) => {
+    try {
+        const searchTerm = req.query.name ? req.query.name.toLowerCase() : null;
+
+        const connection = await createConnection();
+        
+        // Se houver um termo de pesquisa, realizar uma consulta filtrada
+        if (searchTerm) {
+            const [rows, fields] = await connection.execute('SELECT * FROM games WHERE LOWER(name) LIKE ?', [`%${searchTerm}%`]);
+            res.json(rows);
+        } else {
+            // Caso contrário, obter todos os jogos
+            const [rows, fields] = await connection.execute('SELECT * FROM games');
+            res.json(rows);
+        }
+
+        await connection.end();
+    } catch (error) {
+        console.error('Erro ao buscar dados do banco de dados:', error);
+        res.status(500).send('Erro interno do servidor');
     }
 });
 
@@ -58,8 +109,21 @@ app.get('/api/jogos', async (req, res) => {
     }
 });
 
-// Servir arquivos estáticos, incluindo a página index.html na pasta games
-app.use(express.static(path.join(__dirname, 'jogos')));
+app.get('/login', async (req, res) => {
+    res.sendFile(path.join(__dirname, '/login/index.html'));
+});
+
+app.get('/shop', async (req, res) => {
+    res.sendFile(path.join(__dirname, '/shop/index.html'));
+});
+
+app.get('/jogos', async (req, res) => {
+    res.sendFile(path.join(__dirname, '/jogos/index.html'));
+});
+
+app.get('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, '/cadastro/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Servidor Express rodando em http://localhost:${port}`);
